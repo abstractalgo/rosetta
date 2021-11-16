ef78f9d1b14038783b3906b6c0b1f57cfa5aef99
Dragan Okanovic <dragan.okan@gmail.com>
2021-11-16 17:15:51 +0100

    Build fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,7 @@ import RosettaPage from './[...path]';
 
 // for some reason [...path] page doesn't capture the root, so here it is, separately
 const Home: NextPage = () => {
-  return (
-    <RosettaPage
-      availableTechs={[]}
-      files={{}}
-      query={{ topic: null, techs: [] }}
-    />
-  );
+  return <RosettaPage availableTechs={[]} query={{ topic: null, techs: [] }} />;
 };
 
 export default Home;
